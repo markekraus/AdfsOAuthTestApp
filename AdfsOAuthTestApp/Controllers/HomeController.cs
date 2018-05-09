c224598c085a8e597cd1c5a3563bb04280c03f02
Mark Kraus <github@markekraus.com>
2018-05-09 17:06:48 -0500

    Sanitize

---
+++ AdfsOAuthTestApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AdfsOAuthTestApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdfsOAuthTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            ViewData["AdfsLogoutUrl"] = _configuration["AdfsLogoutUrl"];
+            return View();
+        }
+
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        [Authorize]
+        public IActionResult Secure()
+        {
+            ViewData["AdfsLogoutUrl"] = _configuration["AdfsLogoutUrl"];
+            ViewData["UPN"] = User.Claims.Where(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn").FirstOrDefault().Value;
+            return View();
+        }
+
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Signout()
+        {
+            ViewData["AdfsLogoutUrl"] = _configuration["AdfsLogoutUrl"];
+            await AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, "Cookies");
+            return Redirect("/");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AdfsOAuthTestApp/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
+
+namespace AdfsOAuthTestApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(auth =>
+                {
+                    auth.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    auth.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    auth.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(opts =>
+                {
+                    Configuration.GetSection("OpenIdConnect").Bind(opts);
+                    opts.ClientSecret = Configuration["CLIENT_SECRET"];
+                    opts.Events = new OpenIdConnectEvents
+                    {
+                        OnAuthorizationCodeReceived = ctx =>
+                        {
+                            return Task.CompletedTask;
+                        }
+                    };
+                    opts.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true
+                    };
+                });
+
+            services.AddMvc();
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
